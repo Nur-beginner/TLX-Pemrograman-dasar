c4d23e5ba956360708c66e30da79e2733daf9c56
Nur-beginner <mnagamingchannel@gmail.com>
2025-02-10 11:10:50 +0800

    Create C. Perkenalan While.cpp

---
+++ 03. Perulangan/C. Perkenalan While.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int luas_kandang = 12;
+int total = 1;
+    while (total < 4) {
+        luas_kandang += 7;
+        total += 1;
+        cout << luas_kandang << endl;
+
+
+    }
+}
